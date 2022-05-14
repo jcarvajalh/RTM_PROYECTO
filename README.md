c4184bcf5f83209b61581970e88d83acbe171941
jcarvajalh <caamiloc@outlook.com>
2022-05-14 15:39:31 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-mzjrsw
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-mzjrsw)
---
+++ src/components/asig_req.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class AsigReq extends Component {
+  render() {
+    return (
+      <div>
+        <p>Vamos asignar un requerimiento</p>
+      </div>
+    );
+  }
+}
